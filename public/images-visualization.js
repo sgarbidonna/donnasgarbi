0e2791d53a8ce52032644d82200d56f75695c757
D0NN4 <sgarbidonna@gmail.com>
2024-09-30 11:28:09 -0300

    mobile responsive

--- public/images-visualization.js
+++ public/images-visualization.js
@@ -1,37 +1,49 @@
-
-
 const gridItems = document.querySelectorAll('.grid-item');
-
 const previewImage = document.getElementById('preview-image');
 const artTitle = document.getElementById('art-title');
 const artYear = document.getElementById('art-year');
 const artTechnique = document.getElementById('art-technique');
 
-const popup = document.getElementById('popup-gallery');
-const popupImage = document.getElementById('popup-image');
-const closePopup = document.querySelector('.close');
+const updatePreview = (item) => {
+    const newSrc = item.getAttribute('src');
+    previewImage.setAttribute('src', newSrc);
+    artTitle.textContent = item.getAttribute('data-title');
+    artYear.textContent = item.getAttribute('data-year');
+    artTechnique.textContent = item.getAttribute('data-technique');
+};
 
 
 gridItems.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        const newSrc = item.getAttribute('src');
-        previewImage.setAttribute('src', newSrc);
-        artTitle.textContent = item.getAttribute('data-title');
-        artYear.textContent = item.getAttribute('data-year');
-        artTechnique.textContent = item.getAttribute('data-technique');
-        // console.log('cambio, soy el elemento', item.getAttribute('data-title'));
-    });
+    if (window.innerWidth >= 768) {
+        item.addEventListener('mouseover', () => {
+            
+            updatePreview(item);    
+        });
+    } else  {
+        item.addEventListener('touchstart', () => {
+            updatePreview(item);
+        });
+    } 
 });
 
-// Abrir el popup cuando se hace clic en una imagen
-gridItems.forEach(item => {
-    item.addEventListener('click', () => {
-        popupImage.src = item.src;
-        popup.style.display = 'flex';
-    });
-});
 
-// Cerrar el popup cuando se hace clic en la 'X'
-closePopup.addEventListener('click', () => {
-    popup.style.display = 'none';
-});
+
+
+
+// const popup = document.getElementById('popup-gallery');
+// const popupImage = document.getElementById('popup-image');
+// const closePopup = document.querySelector('.close');
+
+
+// // Abrir el popup cuando se hace clic en una imagen
+// gridItems.forEach(item => {
+//     item.addEventListener('click', () => {
+//         popupImage.src = item.src;
+//         popup.style.display = 'flex';
+//     });
+// });
+
+// // Cerrar el popup cuando se hace clic en la 'X'
+// closePopup.addEventListener('click', () => {
+//     popup.style.display = 'none';
+// });
---
+++ public/menu-mobile.js
@@ -0,0 +1,53 @@
+
+function toggleMenu() {
+    const sectionA = document.getElementById('section-a');
+    sectionA.classList.toggle('show');
+}
+
+function closeMenu() {
+    const sectionA = document.getElementById('section-a');
+    sectionA.classList.toggle('show');
+}
+
+
+if (window.matchMedia("(max-width: 1025px)").matches) {
+    document.getElementById('toggle-menu').addEventListener('click', toggleMenu);
+    document.getElementById('close-menu-btn').addEventListener('click', closeMenu);;
+        
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.innerWidth <= 1025) {
+        const mobileMenuBtn = document.getElementById('toggle-menu');
+        const closeMenuBtn = document.getElementById('close-menu-btn');
+        const menu = document.getElementById('section-a');
+
+        if (mobileMenuBtn && closeMenuBtn && menu) {
+            mobileMenuBtn.addEventListener('click', function () {
+                menu.classList.toggle('active');
+            });
+
+            // Cerrar menú
+            closeMenuBtn.addEventListener('click', function () {
+                menu.classList.remove('active');
+                console.log('cerrar');
+            });
+        }
+    }
+});
+
+
+
+// También puedes agregar un event listener para el cambio de tamaño de la ventana
+window.addEventListener('resize', function() {
+    if (window.matchMedia("(max-width: 1025px)").matches) {
+        if (!document.getElementById('toggle-menu').hasAttribute('listener-added')) {
+            document.getElementById('toggle-menu').addEventListener('click', toggleMenu);
+            document.getElementById('toggle-menu').setAttribute('listener-added', true); 
+        }
+    } else {
+        document.getElementById('toggle-menu').removeEventListener('click', toggleMenu);
+        document.getElementById('toggle-menu').removeAttribute('listener-added');
+    }
+});
