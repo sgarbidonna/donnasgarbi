6c1111cf8f0feb095b0a84cc440fa523f7e550b8
D0NN4 <sgarbidonna@gmail.com>
2024-09-29 22:18:58 -0300

    cv y obras

---
+++ public/preloading.js
@@ -0,0 +1,11 @@
+const loadingScreen = document.getElementById('loading-screen');
+const container = document.getElementById('container');
+
+    // Agregar la clase 'show' después de un pequeño retraso
+    setTimeout(() => {
+        loadingScreen.style.opacity = 0; // Oculta la pantalla blanca
+        setTimeout(() => {
+            loadingScreen.style.display = 'none'; // Remueve la pantalla del DOM
+            container.classList.add('show'); // Muestra el contenido
+        }, 1000); // Espera el tiempo de la transición de opacidad
+    }, 100); // Pequeño retraso para permitir que el navegador aplique los estilos iniciales
